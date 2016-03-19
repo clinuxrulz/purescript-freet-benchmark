236c3f1358fd2eee9a33466382e0c0185341efe6
Clinton Selke <clinuxrulz@gmail.com>
2016-03-19 22:02:41 +1000

    initial commit

---
+++ src/CodensityBasedFreeT0cba1b8.purs
@@ -0,0 +1,123 @@
+-- | This module defines a stack-safe implementation of the _free monad transformer_.
+
+module Main.CodensityBasedFreeT0cba1b8
+  ( FreeT()
+  -- , freeT
+  -- , liftFreeT
+  -- , hoistFreeT
+  -- , interpret
+  -- , bimapFreeT
+  -- , resume
+  , runFreeT
+  ) where
+
+import Prelude
+
+import Data.Either (Either(..))
+import Data.Bifunctor (bimap)
+
+import Control.Bind ((<=<))
+import Control.Monad.Rec.Class (MonadRec, tailRecM)
+import Control.Monad.Trans (MonadTrans)
+
+data StacklessF f a next
+  = F (f next)
+  | Suspend (Unit -> next)
+  | Done a
+
+newtype SuspT f m a = SuspT (m (StacklessF f a (SuspT f m a)))
+
+unSuspT :: forall f m a. SuspT f m a -> m (StacklessF f a (SuspT f m a))
+unSuspT (SuspT a) = a
+
+suspend :: forall f m a. (Applicative m) => (Unit -> SuspT f m a) -> SuspT f m a
+suspend thunk = SuspT $ return $ Suspend thunk
+
+done :: forall f m a. (Applicative m) => a -> SuspT f m a
+done a = SuspT $ Done <$> pure a
+
+-- | The free monad transformer for the functor `f`.
+newtype FreeT f m a = FreeT (forall r. (a -> SuspT f m r) -> SuspT f m r)
+
+unFreeT :: forall f m a. FreeT f m a -> (forall r. (a -> SuspT f m r) -> SuspT f m r)
+unFreeT (FreeT a) = a
+
+-- | Construct a computation of type `FreeT`.
+{- TODO: freeT
+--freeT :: forall f m a. (Unit -> m (Either a (f (FreeT f m a)))) -> FreeT f m a
+--freeT = FreeT
+-}
+
+-- | Unpack `FreeT`, exposing the first step of the computation.
+{- TODO: resume
+resume :: forall f m a. (Functor f, MonadRec m) => FreeT f m a -> m (Either a (f (FreeT f m a)))
+resume = tailRecM go
+  where
+  go :: FreeT f m a -> m (Either (FreeT f m a) (Either a (f (FreeT f m a))))
+  go (FreeT f) = map Right (f unit)
+  go (Bind e) = runExists (\(Bound m f) ->
+    case m unit of
+      FreeT m -> do
+        e <- m unit
+        case e of
+          Left a -> return (Left (f a))
+          Right fc -> return (Right (Right (map (\h -> h >>= f) fc)))
+      Bind e1 -> runExists (\(Bound m1 f1) -> return (Left (bind (m1 unit) (\z -> f1 z >>= f)))) e1) e
+--}
+
+instance functorFreeT :: (Applicative m) => Functor (FreeT f m) where
+  map f (FreeT ca) = FreeT (\k -> suspend (\_ -> ca (k <<< f)))
+
+instance applyFreeT :: (Applicative m) => Apply (FreeT f m) where
+  apply (FreeT cf) (FreeT ca) = FreeT (\k -> suspend (\_ -> cf (\f -> suspend (\_ -> ca (k <<< f)))))
+
+instance applicativeFreeT :: (Applicative m) => Applicative (FreeT f m) where
+  pure a = FreeT (\k -> suspend (\_ -> k a))
+
+instance bindFreeT :: (Applicative m) => Bind (FreeT f m) where
+  bind (FreeT ca) f = FreeT (\k -> suspend (\_ -> ca (\a -> (unFreeT $ f a) k)))
+
+instance monadFreeT :: (Applicative m) => Monad (FreeT f m)
+
+instance monadTransFreeT :: MonadTrans (FreeT f) where
+  lift m = FreeT (\k -> SuspT $ do
+    a <- m
+    unSuspT $ k a
+  )
+
+instance monadRecFreeT :: (Applicative m) => MonadRec (FreeT f m) where
+  tailRecM f = go
+    where
+    go s = do
+      e <- f s
+      case e of
+        Left s1 -> go s1
+        Right a -> return a
+
+{-
+-- | Lift an action from the functor `f` to a `FreeT` action.
+liftFreeT :: forall f m a. (Functor f, Monad m) => f a -> FreeT f m a
+liftFreeT fa = FreeT \_ -> return (Right (map pure fa))
+
+-- | Change the underlying `Monad` for a `FreeT` action.
+hoistFreeT :: forall f m n a. (Functor f, Functor n) => (forall b. m b -> n b) -> FreeT f m a -> FreeT f n a
+hoistFreeT = bimapFreeT id
+
+-- | Change the base functor `f` for a `FreeT` action.
+interpret :: forall f g m a. (Functor f, Functor m) => (forall b. f b -> g b) -> FreeT f m a -> FreeT g m a
+interpret nf = bimapFreeT nf id
+
+-- | Change the base functor `f` and the underlying `Monad` for a `FreeT` action.
+bimapFreeT :: forall f g m n a. (Functor f, Functor n) => (forall b. f b -> g b) -> (forall b. m b -> n b) -> FreeT f m a -> FreeT g n a
+bimapFreeT nf nm (Bind e) = runExists (\(Bound a f) -> bound (bimapFreeT nf nm <<< a) (bimapFreeT nf nm <<< f)) e
+bimapFreeT nf nm (FreeT m) = FreeT \_ -> map (nf <<< map (bimapFreeT nf nm)) <$> nm (m unit)
+-}
+
+-- | Run a `FreeT` computation to completion.
+runFreeT :: forall f m a. (MonadRec m) => (forall b. f b -> m b) -> FreeT f m a -> m a
+runFreeT f (FreeT c) = (unSuspT $ c done) >>= tailRecM go
+  where
+    go :: StacklessF f a (SuspT f m a) -> m (Either (StacklessF f a (SuspT f m a)) a)
+    go (F fa) = (f fa) >>= ((Left <$> _) <<< unSuspT)
+    go (Suspend thunk) = Left <$> unSuspT (thunk unit)
+    go (Done a) = return $ Right a
---
+++ src/Main.purs
@@ -0,0 +1,76 @@
+module Main where
+
+import Main.OriginalFreeTe0a15ee as Orig
+import Main.CodensityBasedFreeT0cba1b8 as Cod
+
+import Prelude
+import Data.Array
+import Data.Foldable
+import Data.Monoid.Additive
+import Data.Monoid.Multiplicative
+import Data.Identity
+import Control.Monad.Eff
+import Test.QuickCheck.Arbitrary (arbitrary)
+import Test.QuickCheck.Gen (vectorOf)
+import Benchotron.Core
+import Benchotron.UI.Console
+
+leftBindLargeBenchmark :: Benchmark
+leftBindLargeBenchmark = mkBenchmark
+  { slug: "left-bind-large"
+  , title: "Left associated binds (" ++ show inputsPerSize ++ " input per size)"
+  , sizes: [1, 2, 3, 4, 5, 6, 7] <#> (* 100000)
+  , sizeInterpretation: "Number of binds"
+  , inputsPerSize: inputsPerSize
+  , gen: \n -> vectorOf n (pure 0.0)
+  , functions: [ benchFn "Original FreeT" (runIdentity <<< (Orig.runFreeT id) <<< origBinds)
+               , benchFn "Codensity FreeT" (runIdentity <<< (Cod.runFreeT id) <<< codBinds)
+               ]
+  }
+  where
+  inputsPerSize :: Int
+  inputsPerSize = 1
+
+  origBinds :: Array Number -> Orig.FreeT Identity Identity Number
+  origBinds as = foldl (\b a -> b >>= const (origGen a)) (origGen 0.0) as
+
+  origGen :: forall a. a -> Orig.FreeT Identity Identity a
+  origGen = pure
+
+  codBinds :: Array Number -> Cod.FreeT Identity Identity Number
+  codBinds as = foldl (\b a -> b >>= const (codGen a)) (codGen 0.0) as
+
+  codGen :: forall a. a -> Cod.FreeT Identity Identity a
+  codGen = pure
+
+rightBindLargeBenchmark :: Benchmark
+rightBindLargeBenchmark = mkBenchmark
+  { slug: "right-bind-large"
+  , title: "Right associated binds (" ++ show inputsPerSize ++ " input per size)"
+  , sizes: [1, 2, 3, 4, 5, 6, 7] <#> (* 100000)
+  , sizeInterpretation: "Number of binds"
+  , inputsPerSize: inputsPerSize
+  , gen: \n -> vectorOf n (pure 0.0)
+  , functions: [ benchFn "Original FreeT" (runIdentity <<< (Orig.runFreeT id) <<< origBinds)
+               , benchFn "Codensity FreeT" (runIdentity <<< (Cod.runFreeT id) <<< codBinds)
+               ]
+  }
+  where
+  inputsPerSize :: Int
+  inputsPerSize = 1
+
+  origBinds :: Array Number -> Orig.FreeT Identity Identity Number
+  origBinds as = foldl (\b a -> origGen a >>= const b) (origGen 0.0) as
+
+  origGen :: forall a. a -> Orig.FreeT Identity Identity a
+  origGen = pure
+
+  codBinds :: Array Number -> Cod.FreeT Identity Identity Number
+  codBinds as = foldl (\b a -> codGen a >>= const b) (codGen 0.0) as
+
+  codGen :: forall a. a -> Cod.FreeT Identity Identity a
+  codGen = pure
+
+main = runSuite [ leftBindLargeBenchmark
+                , rightBindLargeBenchmark
+                ]
